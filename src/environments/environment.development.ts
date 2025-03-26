81a3fc52f63a28efc6844ec47afa6b62851b70c6
AdrianGil923 <zS23004688@estuadiantes.uv.mx>
2025-03-26 15:43:40 -0600

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDVb_5lgLQJP_3OMAGJ-FL4BBCSqkLV5Oc",
+    authDomain: "my-cv-e292f.firebaseapp.com",
+    projectId: "my-cv-e292f",
+    storageBucket: "my-cv-e292f.firebasestorage.app",
+    messagingSenderId: "18704728562",
+    appId: "1:18704728562:web:a61c4eba2ffc95a2deeb6b"
+  }
+};
+
